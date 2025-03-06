a812f9fa572f6e98f74befc0808e64329f4c12c1
Sarwar Miral <sarwar.miral@bjitgroup.com>
2025-03-06 14:52:38 +0600

    - Implemented Put and Delete methods. - Implemented basic validations in model class.

--- src/main/java/bjit/academy/jpa_demo/controller/StudentController.java
+++ src/main/java/bjit/academy/jpa_demo/controller/StudentController.java
@@ -21,4 +21,19 @@ public class StudentController {
     public Student findStudent(@RequestParam("email") String email) {
         return studentRepository.findByEmail(email);
     }
+
+    @PutMapping("/student/update")
+    public Student updateStudent(@RequestParam("id") Long id, @RequestParam("new_email") String newEmail){
+        Student student = studentRepository.findById(id).get();
+        student.setEmail(newEmail);
+
+        studentRepository.save(student);
+        return student;
+    }
+
+    @DeleteMapping("/student/delete")
+    public String deleteStudent(@RequestParam("id") Long id){
+        studentRepository.deleteById(id);
+        return "This student has been deleted!";
+    }
 }
--- src/main/java/bjit/academy/jpa_demo/model/Student.java
+++ src/main/java/bjit/academy/jpa_demo/model/Student.java
@@ -1,9 +1,9 @@
 package bjit.academy.jpa_demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Student {
@@ -11,8 +11,15 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto Incrementation
     private Long id;
 
+    @NotNull(message = "Name cannot be null")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
+
+    @NotNull(message = "Email cannot be null")
+    @Email(message = "Email should be valid")
+    @Column(unique = true)
     private String email;
+
     private String contactNo;
 
     public Long getId() {
